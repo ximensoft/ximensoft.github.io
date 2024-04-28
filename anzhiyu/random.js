9b1ff0fffbed310c73020c6c62f3ceaf0a672659
yeluohuakai <455758219@qq.com>
2024-04-29 07:54:24 +0800

    Site updated: 2024-04-29 07:54:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2024/04/4a17b156.html","posts/2024/04/13bb8d58.html","posts/2024/04/2adb98ac.html","posts/2024/04/bbe5d848.html"];function toRandomPost(){
+var posts=["posts/2024/04/4a17b156.html","posts/2024/04/13bb8d58.html","posts/2024/04/2adb98ac.html","posts/2024/04/bbe5d848.html","posts/2024/04/e324621.html","posts/2024/04/0.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
