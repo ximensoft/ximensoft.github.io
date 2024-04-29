e2116ca1669120bc0a98ce9cbdc8cc50bccdd267
yeluohuakai <455758219@qq.com>
2024-04-29 09:18:30 +0800

    Site updated: 2024-04-29 09:18:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/2024/04/2adb98ac.html","posts/2024/04/bbe5d848.html","posts/2024/04/e324621.html"];function toRandomPost(){
+var posts=["posts/2024/04/e324621.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
