1e78cc00d53e42c4297320df06f9c2425339fa19
yeluohuakai <455758219@qq.com>
2024-05-01 10:13:33 +0800

    Site updated: 2024-05-01 10:13:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/cfaf734.html","2024/04/e324621.html","2024/04/e8af88ae.html"];function toRandomPost(){
+var posts=["2024/04/e324621.html","2024/04/e8af88ae.html","2024/04/cfaf734.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
