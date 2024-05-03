59d4199a74631dc378245ea34938a9492b5d5e86
小新 <ximensoft@gmail.com>
2024-05-04 03:07:19 +0800

    Site updated: 2024-05-04 03:07:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/cfaf734.html","2024/05/31bcbaaa.html","2024/05/2d8cd4b0.html","2024/05/1e3efaa4.html","2024/04/e8af88ae.html","2024/04/e324621.html","2024/05/e191016d.html","2024/05/2a084fe0.html","2024/05/816c020.html","2024/05/6ea47570.html"];function toRandomPost(){
+var posts=["2024/04/cfaf734.html","2024/05/31bcbaaa.html","2024/05/2d8cd4b0.html","2024/04/e8af88ae.html","2024/05/1e3efaa4.html","2024/05/e191016d.html","2024/04/e324621.html","2024/05/2a084fe0.html","2024/05/6ea47570.html","2024/05/816c020.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
